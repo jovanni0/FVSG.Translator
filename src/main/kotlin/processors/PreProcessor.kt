a1aa8e3037d73e3def2d9edef8dce360d8efb512
jovanni0 <jovanni0@proton.me>
2026-05-02 21:21:38 +0300

    fix: type 1A,B 2A,B 3A,B spread ellipsis
    Fixed the pre-processor to pass the spread ellipsis test. Also added documentation to the pre-processor to explain the types of spread ellipsis and how the replacements are made.

--- src/main/kotlin/processors/PreProcessor.kt
+++ src/main/kotlin/processors/PreProcessor.kt
@@ -1,6 +1,46 @@
 package processors
 
+/**
+ * processes the Markdown content before it goes into the doc splitter.
+ *
+ * the changes are of type find-replace and aimed at resolving systematic problems in text that do not require complex
+ * logic. they are:
+ * - ellipses: in regular print, ellipsis are usually formed using periods, so they need to be condensed into the
+ *             ellipsis character.
+ */
 class PreProcessor
 {
+    fun run(doc: String): String
+    {
+        return ellipsis(doc)
+    }
 
+
+    /**
+     * handles the replacements related to spread ellipsis (aka ellipsis condensation).
+     *
+     * these are:
+     * - type 1 = spread ellipsis at the end of a sentence. in this case all spaces around are collapsed and the ellipsis
+     *            is moved at the end of the previous word.
+     *     - 1A = the SE have a space at the beginning and the end: `strokes . . . ”`
+     *     - 1B = the SE have a space at the start: `strokes . . .”`
+     * - type 2 = spread ellipsis at the start of a sentence. because it is a symbolic placeholder for previous words,
+     *            it reteins the space after and only the space before is collapsed.
+     *     - 2A = the SE have a space at the beginning and the end: `“ . . . we're`
+     *     - 2B = the SE have a space at the end: `“. . . we're`
+     * - type 3 = generic spread ellipsis in the text. because there is no context about the position in text, it's assumed
+     *            they will be collapsed at the end of the previous word. the trainiling space case is ignored.
+     *     - 3A = the SE have a space at the beginning: `text . . .`
+     *     - 3B = the SE are not padded: `text. . .`
+     */
+    private fun ellipsis(input: String): String
+    {
+        return input
+            .replace(" . . . ”", "…”")  // 1A
+            .replace(" . . .”", "…”")   // 1B
+            .replace("“ . . .", "“…")   // 2A
+            .replace("“. . .", "“…")    // 2B
+            .replace(" . . .", "…")     // 3A
+            .replace(". . .", "…")      // 3B
+    }
 }
