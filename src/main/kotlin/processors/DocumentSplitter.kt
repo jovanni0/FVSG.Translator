43e4f9eaea67f1c83a6dc51f350da0dd1923910f
jovanni0 <jovanni0@proton.me>
2026-05-03 14:38:10 +0300

    docs: added documentation to DocumentSplitter

--- src/main/kotlin/processors/DocumentSplitter.kt
+++ src/main/kotlin/processors/DocumentSplitter.kt
@@ -7,38 +7,28 @@ import data_types.Sentence
 import data_types.Word
 import data_types.wordFromString
 
+
+/**
+ * splits the document into its components:
+ * - fragment
+ * - line
+ * - sentence
+ *
+ * each component is defined by a character/sequesnce which is used as the boundary indicator.
+ */
 class DocumentSplitter
 {
-//    fun splitDoc(doc: String): Document
-//    {
-//        val raw_fragments = doc.split(Regex("(\\r?\\n\\s*){2,}"))
-//        val fragments = mutableListOf<Fragment>()
-//
-//        for (fragment in raw_fragments)
-//        {
-//            val lines = splitFragment(fragment)
-//            fragments.add(Fragment(lines))
-//        }
-//
-//        return Document(fragments)
-//    }
-//
-//
-//    fun splitFragment(fragment: String): List<Line>
-//    {
-//        val raw_lines = fragment.split(Regex("\\r?\\n"))
-//        val lines = mutableListOf<Line>()
-//
-//        for (line in raw_lines)
-//        {
-//            val sentences = splitLine(line)
-//            lines.add(sentences)
-//        }
-//
-//        return lines
-//    }
+    val fragment_boundary = Regex("(\\r?\\n\\s*){2,}")
+    val line_boundary = Regex("\\r?\\n")
+    val word_boundary = Regex("\\s+")
 
-
+    /**
+     * the list of characters considered markers of sentence end.
+     *
+     * this list contains indicators of possible sentence end. because the process is aggressive, every character that
+     * can be used to separate sentences will be used to split the line into sentences. this way a namecase word will
+     * not be confused with a name because it's the start of a sentence.
+     */
     val end_of_sentence = setOf(
         '.',
         '?',
@@ -48,23 +38,33 @@ class DocumentSplitter
     )
 
 
+    /**
+     * split a document into it's components (fragments, lines, sentences).
+     */
     fun splitDoc(doc: String): Document
     {
         val fragments = doc
-            .split(Regex("(\\r?\\n\\s*){2,}"))
+            .split(fragment_boundary)
             .map { fragment -> Fragment(splitFragment(fragment)) }
 
         return Document(fragments)
     }
 
+
+    /**
+     * split a fragment into lines.
+     */
     fun splitFragment(fragment: String): List<Line>
     {
         return fragment
-            .split(Regex("\\r?\\n"))
+            .split(line_boundary)
             .map { line -> splitLine(line) }
     }
 
 
+    /**
+     * split a line into sentences.
+     */
     fun splitLine(line: String): Line
     {
         val sentences = mutableListOf<Sentence>()
@@ -90,14 +90,22 @@ class DocumentSplitter
     }
 
 
+    /**
+     * check if the string contains any characters defined in `end_of_sentence`.
+     * return `True` if contains, else `False`.
+     */
     fun String.isSentenceEnd(): Boolean
     {
         return this.any { it in end_of_sentence }
     }
 
 
+    /**
+     * split a string into words, discarding whitespaces and empty strings.
+     * the splitting is done based on `word_boundary` Regex.
+     */
     fun String.splitIntoWords(): List<String>
     {
-        return this.split(Regex("\\s+")).filter { it.isNotEmpty() }
+        return this.split(word_boundary).filter { it.isNotEmpty() }
     }
 }
